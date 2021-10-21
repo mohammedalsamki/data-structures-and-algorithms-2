cca067703c33413ebbb41455aab1c555a02c0afb
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-10-21 16:13:19 +0300

    build : Queue Implementation done with tests

--- python/stack_and_queue/README.md
+++ python/stack_and_queue/README.md
@@ -35,9 +35,32 @@ Implementation of Stacks and Queues
         - Arguments: none
         - Returns: Boolean indicating whether or not the stack is empty.
 
+### Queue
+
+* [x] Create a Queue class that has a front property. It creates an empty Queue when instantiated.
+    - [x] This object should be aware of a default empty value assigned to front when the queue is created.
+* [x] The class should contain the following methods:
+    - [x] enqueue
+        - Arguments: value
+        - adds a new node with that value to the back of the queue with an O(1) Time performance.
+
+    - [x]dequeue
+        - Arguments: none
+        - Returns: the value from node from the front of the queue
+        - Removes the node from the front of the queue
+        - Should raise exception when called on empty queue
+    - [x] peek
+        - Arguments: none
+        - Returns: Value of the node located at the front of the queue
+        - Should raise exception when called on empty stack
+    - [x] is empty
+        - Arguments: none
+        - Returns: Boolean indicating whether or not the queue is empty
+
 ## Test Requirements
 
-* Stack
+### Stack
+
     - [x] Can successfully push onto a stack
     - [x] Can successfully push multiple values onto a stack
     - [x] Can successfully pop off the stack
@@ -46,6 +69,17 @@ Implementation of Stacks and Queues
     - [x] Can successfully instantiate an empty stack
     - [x] Calling pop or peek on empty stack raises exception
 
+### Queue
+
+    - [x] Can successfully enqueue into a queue
+    - [x] Can successfully enqueue multiple values into a queue
+    - [x] Can successfully dequeue out of a queue the expected value
+    - [x] Can successfully peek into a queue, seeing the expected value
+    - [x] Can successfully empty a queue after multiple dequeues
+    - [x] Can successfully instantiate an empty queue
+    - [x] Calling dequeue or peek on empty queue raises exception
 # White Board
 
 ![Stacks](Stacks.jpg)
+
+![Queues](Queues.jpg)
--- python/stack_and_queue/stack_and_queue.py
+++ python/stack_and_queue/stack_and_queue.py
@@ -2,16 +2,20 @@ class Node:
     """
     The Node class which will be a part of stack
     """
+
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
+
 
 class Stack(Node):
     """
     The Stack class which will have the top field
     """
+
     def __init__(self, top=None):
         self.top = top
+
     def push(self, value):
         """
         The Push Method for adding value to the stack
@@ -21,6 +25,7 @@ class Stack(Node):
         node = Node(value)
         node.next = self.top
         self.top = node
+
     def pop(self):
         """
         The Pop Method for removing value to the stack
@@ -33,6 +38,7 @@ class Stack(Node):
         self.top = self.top.next
         temp.next = None
         return temp.value
+
     def peek(self):
         """
         The Peek Method for showing to top value in the stack
@@ -40,10 +46,60 @@ class Stack(Node):
         Return: value at the top of stack with any type
         """
         return self.top.value
+
     def is_empty(self):
         """
-        The Is Empty Method for showing to top value in the stack
+        The Is Empty Method for Check if the Stack is Empty
         Arg   : No Arguments
         Return: True if the stack is empty and False if stack is not empty
         """
         return not self.top
+
+
+class Queue(Node):
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        """
+        The Enqueue Method for adding value to the rear of Queue
+        Arg   : Value with any type
+        Return: No return value
+        """
+        node = Node(value)
+        if not self.rear:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+
+    def dequeue(self):
+        """
+        The Pop Method for removing value from front of the Queue
+        Arg   : No Arguments
+        Return: value beign removed with any type
+        """
+        if self.front == None:
+            raise Exception("This stack is empty")
+        temp = self.front
+        self.front = self.front.next
+        temp.next = None
+        return temp.value
+
+    def peek(self):
+        """
+        The Peek Method for showing to front value in the Queue
+        Arg   : No Arguments
+        Return: value at the front of Queue with any type
+        """
+        return self.front.value
+
+    def is_empty(self):
+        """
+        The Is Empty Method for Check if the Queue is Empty
+        Arg   : No Arguments
+        Return: True if the Queue is empty and False if Queue is not empty
+        """
+        return not self.front
