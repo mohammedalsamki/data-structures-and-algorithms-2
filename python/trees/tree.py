91b6acd93b0d343fba0472f4a82743efb207aa5b
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-10-28 18:20:44 +0300

    build : Queue Class added

--- python/trees/tree.py
+++ python/trees/tree.py
@@ -3,3 +3,18 @@ class Node:
         self.value = data
         self.left = left
         self.right = right
+
+class Queue:
+    def __init__(self, collection=[]):
+        self.data = collection
+
+    def peek(self):
+        if len(self.data):
+            return True
+        return False
+
+    def enqueue(self, item):
+        self.data.append(item)
+
+    def dequeue(self):
+        return self.data.pop(0)
