09e7a8f00b4a3df0ae6d1d64608f5966efbd71ce
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-10-31 15:54:49 +0200

    test: Find max method tests added

--- python/tests/test_tree.py
+++ python/tests/test_tree.py
@@ -5,7 +5,6 @@ Binary Tree Tests
 from trees.tree import BinaryTree, Node, BinarySearchTree
 import pytest
 
-
 def test_bfs(tree_builder):
     expected = ["A", "B", "C", "D"]
     actual = tree_builder.bfs()
@@ -33,12 +32,10 @@ def test_bst_root_addition():
     actual = bst.root.value
     assert actual == expected
 
-
 def test_bst_mulitple_values_addition(addition):
     assert addition.root.value == 22
     assert addition.root.right.value == 55
     assert addition.root.left.value == 10
-
 
 def test_bst_four_values_addition(addition):
     assert addition.root.value == 22
@@ -46,8 +43,10 @@ def test_bst_four_values_addition(addition):
     assert addition.root.left.value == 10
     assert addition.root.left.right.value == 11
 
-
-
+def test_bst_find_Max(build_tree_for_max):
+    actual=build_tree_for_max.findMax(build_tree_for_max.root)
+    expected=11
+    assert actual==expected
 
 @pytest.fixture
 def addition():
@@ -69,5 +68,27 @@ def tree_builder():
     a_node.right = c_node
     b_node.left = d_node
     tree.root = a_node
-
     return tree
+
+@pytest.fixture
+def build_tree_for_max():
+    tree = BinaryTree()
+    a_node = Node(2)
+    b_node = Node(7)
+    c_node = Node(5)
+    d_node = Node(2)
+    e_node = Node(6)
+    f_node = Node(9)
+    g_node = Node(4)
+    h_node = Node(11)
+    a_node.left = b_node
+    a_node.right = c_node
+    b_node.left = d_node
+    b_node.right = e_node
+    c_node.right = f_node
+    e_node.left = f_node
+    e_node.right = h_node
+    f_node.left = g_node
+    tree.root = a_node
+    return tree
+
