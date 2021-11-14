145adfb7c5fb845c9b952c0f310020357d7c40b7
Jehad Abu Awwad <jehadabuawwad@outlook.com>
2021-11-14 20:26:49 +0200

    feat : insertion sort method updated

--- python/insertion_sort/insertion_sort.py
+++ python/insertion_sort/insertion_sort.py
@@ -1,14 +1,12 @@
 def insertion_sort(list):
 
     sorted=list
-    for ind,itm in enumerate(sorted):
+    for ind in range(1,len(sorted)):
         k=ind-1
-        temp=itm
-
+        temp=sorted[ind]
+        print(ind,k,temp,sorted)
         while k>=0 and temp <list[k]:
             list[k+1] = list[k]
             k-=1
         list[k+1]=temp
     return sorted
-
-
