0004120aaa86e22cc9f8f6e47d0191d61cc0af91
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-01 12:48:33 +0200

    fix: fixing the access for HashTable attributes and methods

--- python/hashmap_left_join/hash_map_left_join.py
+++ python/hashmap_left_join/hash_map_left_join.py
@@ -8,21 +8,21 @@ def left_join(table_1, table_2):
         output: a list of values after left join.
     """
     output = []
-    table_1_buckets=table_1._Hashtable__buckets
+    table_1_buckets=table_1._HashTable__buckets
     for item in table_1_buckets:
         if item:
             current = item.head
             while current:
-                hashed = table_1._Hashtable__hash(current.value[0])
-                target = table_2._Hashtable__buckets[hashed]
+                hashed = table_1._HashTable__hash(current.value[0])
+                target = table_2._HashTable__buckets[hashed]
                 if not target:
                     output.append((current.value[0], (current.value[1], None)))
-                    current = current._next
+                    current = current.next
                     continue
                 target = target.head
                 while target:
                     if target.value[0] == current.value[0]:
                         output.append((current.value[0], (current.value[1], target.value[1])))
-                    target = target._next
-                current = current._next
+                    target = target.next
+                current = current.next
     return output
